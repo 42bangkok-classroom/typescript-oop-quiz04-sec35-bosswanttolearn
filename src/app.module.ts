cb66db5c16dcb7620da3751d05a860a5606fbcf2
bosswanttolearn <68011820@kmitl.ac.th>
2026-03-02 10:15:59 +0700

    03

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
-
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [],
